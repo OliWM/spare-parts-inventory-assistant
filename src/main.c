e78834972c01a39994a8aacb367a279b3e40b6fb
OliWM <oliver@climaider.com>
2025-09-22 22:04:36 +0200

    Fixed count, made more robust and able to add more products etc.

--- src/main.c
+++ src/main.c
@@ -1,33 +1,47 @@
 // main.c
 // inform user if the tools they need are in our inventory or not.
 
+#include "stdbool.h"
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
 #define MAX_INPUT 100
-int main() {
-  const char part1[] = "hydraulic pump";
-  const char part2[] = "PLC module";
-  const char part3[] = "servo motor";
+int main(void) {
+  const char *parts[] = {"hydraulic pump", "PLC module", "servo motor"};
   const char special1[] = "Do you actually have any parts?";
   const char special2[] = "Is there anything in stock at all?";
   char userPart[MAX_INPUT] = "";
-
+  int partCount = sizeof(parts) / sizeof(parts[0]);
+  bool found = false;
   puts("Hej. Welcome to the spare parts inventory!");
-  while (1) {
+
+  while (found != true) {
     puts("Which part do you need?");
     fgets(userPart, sizeof(userPart), stdin);
     userPart[strcspn(userPart, "\n")] = '\0';
-    if ((strcmp(userPart, part1) == 0) || (strcmp(userPart, part2) == 0) ||
-        (strcmp(userPart, part3) == 0)) {
-      printf("I have got %s here for you 😊. Bye!\n", userPart);
-      break;
-    } else if ((strcmp(userPart, special1) == 0) ||
-               (strcmp(userPart, special2) == 0)) {
-      printf("We have {part_count} part(s)!%s\n%s\n%s\n", part1, part2,
-             part3); // lav rent faktisk et part count
+
+    if ((strcmp(userPart, special1) == 0) ||
+        (strcmp(userPart, special2) == 0)) {
+      printf("We have %d part(s)!\n", partCount);
+      for (size_t n = 0; n < partCount; n++) {
+        printf("%s\n", parts[n]);
+      }
+      continue;
     } else {
+      for (size_t i = 0; i < partCount; i++) {
+        if (strcmp(parts[i], userPart) == 0) {
+          printf("I have got %s here for you 😊. Bye!\n", userPart);
+          found = true;
+          break;
+        }
+      }
+    }
+    if (found != true) {
       printf("I am afraid we don’t have any %s in the inventory 😔\n",
              userPart);
+      continue;
     }
   }
   return 0;
